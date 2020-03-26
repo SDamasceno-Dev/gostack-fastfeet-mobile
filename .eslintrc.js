41a6d2dda263948aa452acf618eaada901ca1d9c
sdamasceno-dev <sdamasceno.dev@gmail.com>
2020-03-26 16:04:33 -0300

    React-navigation implemented

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,5 +54,12 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src'
+      }
+    }
   }
 };
