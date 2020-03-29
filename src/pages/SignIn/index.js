af81addbe5348e299f74ecb8687ec2dd93969780
sdamasceno-dev <sdamasceno.dev@gmail.com>
2020-03-28 23:05:14 -0300

    SignIn with Redux OK

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -3,14 +3,24 @@
  * @description: SignIn page of App
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 import logo from '~/assets/FastFeet_logo.png';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+
+  const [id, setId] = useState('');
+
+  function handleSubmit() {
+    dispatch(signInRequest(id));
+  }
   return (
     <Container>
       <Image source={logo} />
@@ -21,8 +31,11 @@ export default function SignIn() {
           keyboardType="number-pad"
           returnKeyType="send"
           placeholder="Informe seu ID de cadastro"
+          value={id}
+          onChangeText={setId}
+          onSubmitEditing={handleSubmit}
         />
-        <SubmitButton onPress={() => {}} style={{}}>
+        <SubmitButton onPress={handleSubmit} style={{}}>
           Entrar no Sistema
         </SubmitButton>
       </Form>
