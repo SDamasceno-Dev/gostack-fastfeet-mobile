4418e98867347ac01b62dd1b73192b10940eba8b
sdamasceno-dev <sdamasceno.dev@gmail.com>
2020-03-28 21:44:04 -0300

    Reactotron configured

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 
 export default function App() {
