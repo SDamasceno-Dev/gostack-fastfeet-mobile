06d06f6de23be7b24f42c2409665effa5914631a
sdamasceno-dev <sdamasceno.dev@gmail.com>
2020-04-04 19:02:57 -0300

    Camera take pictures

--- src/pages/DeliveryConfirm/index.js
+++ src/pages/DeliveryConfirm/index.js
@@ -1,8 +1,99 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, StyleSheet, Alert, CameraRoll } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Permissions from 'react-native-permissions';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container,
+  Background,
+  BtnCamera,
+  BtnSubmitDelivery,
+  BtnDescription,
+} from './styles';
 
 export default function DeliveryConfirm() {
-  return <View />;
+  const [permission, setPermission] = useState('undetermined');
+
+  console.tron.log(permission);
+
+  useEffect(() => {
+    Permissions.check('android.permission.CAMERA').then((response) => {
+      // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+      setPermission(response);
+    });
+  }, []);
+
+  async function takePicture(camera) {
+    if (camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await camera.takePictureAsync(options);
+      console.tron.log(data.uri);
+    }
+  }
+
+  const styles = StyleSheet.create({
+    preview: {
+      alignSelf: 'center',
+      width: 335,
+      top: 140,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  return (
+    <Container>
+      <Background />
+      <RNCamera
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        captureAudio={false}>
+        {({ camera }) => {
+          console.tron.log('enterCamera', permission);
+          return (
+            <BtnCamera
+              onPress={() => {
+                if (permission !== 'granted') {
+                  Alert.alert(
+                    'Permissão necessária',
+                    'É preciso garantir o acesso à câmera para utilizar esse recurso!'
+                  );
+                } else {
+                  takePicture(camera);
+                }
+              }}>
+              <Icon name="photo-camera" size={35} color="#fff" />
+            </BtnCamera>
+          );
+        }}
+      </RNCamera>
+      <BtnSubmitDelivery>
+        <BtnDescription>Enviar</BtnDescription>
+      </BtnSubmitDelivery>
+    </Container>
+  );
 }
+
+DeliveryConfirm.navigationOptions = (data) => ({
+  headerTransparent: true,
+  headerTintColor: '#fff',
+  headerTitleAlign: 'center',
+  title: 'Confirmar entrega',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        data.navigation.goBack();
+      }}>
+      <Icon name="chevron-left" size={30} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
--- src/pages/DeliveryConfirm/styles.js
+++ src/pages/DeliveryConfirm/styles.js
@@ -1,3 +1,39 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Background = styled.View`
+  height: 155px;
+  background: #7d40e7;
+`;
+
+export const BtnCamera = styled(RectButton)`
+  width: 62px;
+  height: 62px;
+  border-radius: 31px;
+  background: rgba(0, 0, 0, 0.3);
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  top: 170px;
+`;
+
+export const BtnSubmitDelivery = styled(RectButton)`
+  top: 350px;
+  width: 335px;
+  height: 45px;
+  border-radius: 4px;
+  background: #7d40e7;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+`;
+
+export const BtnDescription = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+`;
